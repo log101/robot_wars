9922ed4ffdcb30b4eff5829164437034719f27da
Nurulhak Sütdede <65677585+nurulhakS@users.noreply.github.com>
2022-09-28 22:50:52 +0300

    2 robot can fight. Besides user can control own robot

--- first_view/main.go
+++ first_view/main.go
@@ -4,8 +4,11 @@ import (
 	"fmt"
 	"math/rand"
 	"strconv"
+	"time"
 )
 
+//------
+//robotun genel özellikleri
 type Robot struct {
 	Name            string
 	heal            int16
@@ -16,35 +19,68 @@ type Robot struct {
 	height          uint8
 	volume          uint16
 	dodgePossibilty uint8
+	isDodgeSkill    bool
 }
 
-func (r Robot) Skill1(robot2 Robot) Robot {
-
-	//robot2 = robot2.heal - int16(r.physicalPower)
-
-	return robot2
+//atak kabiliyeti
+func (r Robot) AtackSkill(enemyRobot Robot) (Robot, Robot) {
+	randomNumber := uint8(rand.Intn(7)) + enemyRobot.dodgePossibilty/3
+	if randomNumber < 7 {
+		fmt.Println(r.Name, r.physicalPower, "hasar vurdu")
+		enemyRobot.heal = enemyRobot.heal - int16(r.physicalPower)
+	} else {
+		fmt.Println(enemyRobot.Name, " sıyrıldı")
+	}
+	if r.isDodgeSkill == true {
+		r.dodgePossibilty /= 2
+	}
+	r.isDodgeSkill = false
+	return r, enemyRobot
 }
 
-var Deneme = "a"
-
-func (r Robot) SetRobotProperty(name string, heal int16, speed uint8, physicalPower uint8, width uint8, length uint8, height uint8) Robot {
-	r.Name = name
-	r.heal = heal
-	r.speed = speed
-	r.physicalPower = physicalPower
-	r.width = width
-	r.length = length
-	r.height = height
-	r.volume = uint16(width) * uint16(length) * uint16(height)
-	r.dodgePossibilty = uint8(10000/r.volume + uint16(speed))
+//can basma kabiliyeti
+func (r Robot) HealSkill() Robot {
+	if r.isDodgeSkill == true {
+		r.dodgePossibilty /= 2
+	}
+	r.heal += 25
+	r.isDodgeSkill = false
+	fmt.Println(r.Name, "Can Bastı")
 	return r
 }
 
+//kaçınma kabiliyeti
+func (r Robot) DodgeSkill() Robot {
+	if r.isDodgeSkill == true {
+		r.dodgePossibilty /= 2
+	}
+	r.dodgePossibilty *= 2
+	r.isDodgeSkill = true
+	fmt.Println(r.Name, "kaçış ihtimalini yükseltti")
+	return r
+}
+
+func (r Robot) SetRobotProperty(name string) Robot {
+	r.Name = name
+
+	r.heal = int16(rand.Intn(21) + 80)          // heal
+	r.speed = uint8(rand.Intn(5) + 1)           // speed
+	r.physicalPower = uint8(rand.Intn(21) + 20) // physical power
+	r.width = uint8(rand.Intn(15) + 15)         // width
+	r.length = uint8(rand.Intn(15) + 15)        // length
+	r.height = uint8(rand.Intn(15) + 15)        // height
+	r.volume = uint16(r.width) * uint16(r.length) * uint16(r.height)
+	r.dodgePossibilty = uint8(10000/r.volume + uint16(r.speed))
+	return r
+}
+
+//------
+
 func main() {
 	var chosen int8
-	var robots []Robot //robot struct'ından türeyecek
-	robots = createRobot(robots, 10)
-	//robots = createYourRobot(robots)
+	var robots []Robot
+	robots = createRobot(robots, 10, "")
+	robots = createYourRobot(robots)
 OuterLoop:
 	for {
 		fmt.Print("\nMENÜ \n------- \n0-Menü \n1-Kendi robotun \n2-Tüm robotlar \n3-Ligler \n4-Sıradaki rakip \n5-Dövüş \n6-Oyundan Çık\n")
@@ -58,12 +94,12 @@ OuterLoop:
 				}
 			case 1:
 				{
-					fmt.Println(robots[len(robots)-1])
+					printRobotList(robots, true)
 					chosen = -1
 				}
 			case 2:
 				{
-					printRobotList(robots)
+					printRobotList(robots, false)
 					chosen = -1
 				}
 			case 4:
@@ -72,8 +108,7 @@ OuterLoop:
 				}
 			case 5:
 				{
-					fmt.Println("Fight")
-					chosen = -1
+					Fight(robots[len(robots)-1], robots[0], true)
 				}
 			case 6:
 				{
@@ -90,42 +125,132 @@ OuterLoop:
 	}
 }
 
-func createRobot(robots []Robot, countOfRobot int) []Robot {
-	for i := 0; i < countOfRobot; i++ {
-		//robotun değerleri ayarlanacak
-		name := "robot" + strconv.Itoa(i)
-		var heal int16 = int16(rand.Intn(21) + 80)          // heal
-		var speed uint8 = uint8(rand.Intn(5) + 1)           // speed
-		var physicalPower uint8 = uint8(rand.Intn(21) + 20) // physical power
-		var width uint8 = uint8(rand.Intn(15) + 15)         // width
-		var length uint8 = uint8(rand.Intn(15) + 15)        // length
-		var height uint8 = uint8(rand.Intn(15) + 15)        // height
-		var newRobot Robot
-		newRobot = newRobot.SetRobotProperty(name, heal, speed, physicalPower, width, length, height)
+//--------
+//Robot oluşturma ve ekrana bilgilerini yazdırma
+func createRobot(robots []Robot, countOfRobot int, name string) []Robot {
 
-		//en son robotlar eklenecek
+	for i := 0; i < countOfRobot; i++ {
+		if name == "" {
+			name = "robot" + strconv.Itoa(i+1)
+		}
+		var newRobot Robot
+		newRobot = newRobot.SetRobotProperty(name)
 		robots = append(robots, newRobot)
+		name = ""
 	}
 	return robots
 }
 
-func printRobotList(robots []Robot) {
-	for i := 0; i < len(robots); i++ {
-		//isim güç falan yazılacak burada ekrana
-		fmt.Println(robots[i].Name)
-		fmt.Println(robots[i].heal)
-		fmt.Println(robots[i].speed)
-		fmt.Println(robots[i].physicalPower)
-		fmt.Println(robots[i].volume)
-		fmt.Println(robots[i].dodgePossibilty)
+func printRobotList(robots []Robot, isPlayerRobot bool) {
+	if isPlayerRobot {
+		fmt.Println("İsmi: ", robots[len(robots)-1].Name)
+		fmt.Println("Canı: ", robots[len(robots)-1].heal)
+		fmt.Println("Fiziksel Gücü: ", robots[len(robots)-1].physicalPower)
+		fmt.Println("Kaçınma Olasılığı: ", robots[len(robots)-1].dodgePossibilty)
+		fmt.Println("---------")
+	} else {
+		for i := 0; i < len(robots); i++ {
+			fmt.Println("İsmi: ", robots[i].Name)
+			fmt.Println("Canı: ", robots[i].heal)
+			fmt.Println("Fiziksel Gücü: ", robots[i].physicalPower)
+			fmt.Println("Kaçınma Olasılığı: ", robots[i].dodgePossibilty)
+			fmt.Println("---------")
+		}
 	}
 }
 
 func createYourRobot(robots []Robot) []Robot {
-	var playerRobot Robot //robot struct'tından türeyecek
+	var playerRobot Robot
 	fmt.Print("Robotunun ismi : ")
 	fmt.Scanf("%s\n", &playerRobot.Name)
-	//fmt.Scanf("%s\n", &playerRobot)
-	robots = append(robots, playerRobot)
+	robots = createRobot(robots, 1, playerRobot.Name)
 	return robots
+}
+
+//--------
+
+//--------
+//Arena kısmı
+
+func Fight(robot1 Robot, robot2 Robot, isPlayerRobot bool) {
+	var playerChosenSkill uint8
+	fmt.Println("Sol Köşede: ", robot1.Name)
+	fmt.Println("Sağ Köşede: ", robot2.Name)
+	for {
+		if !isPlayerRobot {
+			time.Sleep(5 * time.Second)
+			robot1, robot2 = robotUseSkill(robot1, robot2, 0)
+
+			fmt.Println("İsmi: ", robot1.Name)
+			fmt.Println("Canı: ", robot1.heal)
+			fmt.Println("Kaçınma Olasılığı: ", robot1.dodgePossibilty)
+			fmt.Println("---------")
+		} else {
+			fmt.Println("Hangi Skill'i kullanmak istiyorsun 1-Attack 2-Heal 3-DoubleDodge")
+			for {
+				fmt.Scanf("%d\n", &playerChosenSkill)
+				if playerChosenSkill < 0 || playerChosenSkill > 3 {
+					fmt.Println("Yanlış sayı girdiniz lütfen 1,2 veya 3 den bir tanesini giriniz.")
+				} else {
+					break
+				}
+			}
+			robot1, robot2 = robotUseSkill(robot1, robot2, playerChosenSkill)
+
+			fmt.Println("İsmi: ", robot1.Name)
+			fmt.Println("Canı: ", robot1.heal)
+			fmt.Println("Kaçınma Olasılığı: ", robot1.dodgePossibilty)
+			fmt.Println("---------")
+		}
+
+		if isExplosion(robot2) {
+			fmt.Println(robot2.Name, " Patladı")
+			break
+		}
+		fmt.Println("Rakip bekleniyor")
+		time.Sleep(5 * time.Second)
+		robot2, robot1 = robotUseSkill(robot2, robot1, 0)
+		if isExplosion(robot1) {
+			fmt.Println(robot1.Name, " Patladı")
+			break
+		}
+		fmt.Println("İsmi: ", robot2.Name)
+		fmt.Println("Canı: ", robot2.heal)
+		fmt.Println("Kaçınma Olasılığı: ", robot2.dodgePossibilty)
+		fmt.Println("---------")
+	}
+}
+
+func robotUseSkill(robot1 Robot, robot2 Robot, playerChosenSkill uint8) (Robot, Robot) {
+
+	if playerChosenSkill == 0 {
+		diceValue := rollToDice(25)
+
+		if diceValue <= 15 {
+			return robot1.AtackSkill(robot2)
+		} else if diceValue <= 20 {
+			return robot1.HealSkill(), robot2
+		} else {
+			return robot1.DodgeSkill(), robot2
+		}
+	} else {
+		if playerChosenSkill == 1 {
+			return robot1.AtackSkill(robot2)
+		} else if playerChosenSkill == 2 {
+			return robot1.HealSkill(), robot2
+		} else {
+			return robot1.DodgeSkill(), robot2
+		}
+	}
+}
+
+func isExplosion(robot Robot) bool {
+	if robot.heal <= 0 {
+		return true
+	}
+	return false
+}
+
+func rollToDice(faceOfDice int) int8 {
+	return int8(rand.Intn(faceOfDice) + 1)
 }
